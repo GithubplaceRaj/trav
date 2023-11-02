5e72a822b4b2f3b313845243292fab34fd91341d
Rajarshi Sinha <132085137+GithubplaceRaj@users.noreply.github.com>
2023-11-02 19:32:12 +0530

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel="stylesheet" type="text/css" href="gallery.css"/>
+    <script src='main.js'></script>
+</head>
+<style>
+    body{
+    background-image: url("back1.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 0%;
+    background-attachment: fixed; 
+    }
+</style>
+<body>
+<?php include("nav.php"); ?>
+
+<div class="image-container">
+        <div class="img-item">
+            <img src="1.jpg" class="single-img" width="400px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="2.jpg" class="single-img" width="400px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="3.jpg" class="single-img" width="300px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="4.jpg" class="single-img" width="350px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+            <img src="5.jpg" class="single-img" width="300px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="6.jpg" class="single-img" width="400px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="7.jpg" class="single-img" width="250px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="8.jpg" class="single-img" width="350px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="9.jpg" class="single-img" width="350px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="10.jpg" class="single-img" width="350px" style=" border-radius: 5px;">
+        </div>
+        <div class="img-item">
+           <img src="11.jpg" class="single-img" width="350px" style=" border-radius: 5px;">
+        </div>
+  
+</div>
+    
+</body>
+</html>
